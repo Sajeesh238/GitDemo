1661c04302dfc5c613b261f76fabbba331c64246
Sajeesh Pandhaloor <sajeesh238@gmail.com>
2021-10-05 20:28:37 +0530

    this is commit from different time zone

--- src/test/java/com/rahulshettyacademy/RestAPITest.java
+++ src/test/java/com/rahulshettyacademy/RestAPITest.java
@@ -9,6 +9,9 @@ public class RestAPITest {
     public void postJira() {
 
         System.out.println("JiraTickets");
+        System.out.println("JiraTickets2");
+        System.out.println("JiraTickets3");
+        System.out.println("JiraTickets4");
     }
 
     @Test
